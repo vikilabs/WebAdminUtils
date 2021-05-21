51860d7afa179f37486542ad04e1440900630a8e
Vignesh Natarajan <vickyemailbox@gmail.com>
2021-05-21 22:17:16 +0000

    migration test case

--- unit_test/run_ut.sh
+++ unit_test/run_ut.sh
@@ -140,20 +140,40 @@ function verify_restore_test_2()
 
 function migrate_test_1()
 {
-	cd sandbox
+    cd $CURRENT_DIR/sandbox
 	[ $? -ne 0 ] && { echo "[ UT_ERROR ] [ ${LINENO} ] "; exit 1; }		
-	echo "[ STATUS  ] backup_test_1"
-	./migrate_website $BACKUP_DIR/backup.tar.gz
-
-	echo "[ SUCCESS ] backup_test_1"
+	echo "[ STATUS  ] migrate_test_1"
+    ./migrate_website "$BACKUP_DIR/backup.tar.gz"
+	[ $? -ne 0 ] && { echo "[ UT_ERROR ] [ ${LINENO} ] "; exit 1; }		
+	echo "[ SUCCESS ] migrate_test_1"
 	cd $CURRENT_DIR
 	[ $? -ne 0 ] && { echo "[ UT_ERROR ] [ ${LINENO} ] "; exit 1; }		
+}
+
+function verify_migrate_test_1()
+{
+    cd $CURRENT_DIR
+    ./test_case_verify_migrate_1.sh    
+	[ $? -ne 0 ] && { echo "[ ERROR ] [ ${LINENO} ] verify_migrate_test_1 failed"; exit 1; }		
+    echo "[ SUCCESS ] verify_migrate_test_1()"
+}
+
+function clear_website_and_db()
+{
+    #clean website content
+    cd $WEBSITE_ROOT_DIR
+	[ $? -ne 0 ] && { echo "[ UT_ERROR ] [ ${LINENO} ] "; exit 1; }		
+    rm -rf *
+
+    #clean DB
+    mysql -u$DB_USERNAME -p$DB_PASSWORD -h $DB_HOST -P $DB_PORT $DB_NAME -e "DROP TABLE test_table;"
 }
 
 function cleanup()
 {
 	cd $CURRENT_DIR
     rm -rf sandbox
+    rm -rf test_website
 }
 
 function main()
@@ -165,13 +185,20 @@ function main()
     import_config
 
     #Do not change the order of test case execution
-    #success restore_test_1
-    #success verify_restore_test_1
-    #success backup_test_1
-    #success verify_backup_test_1
+    clear_website_and_db
+    restore_test_1
+    verify_restore_test_1
+
+    backup_test_1
+    verify_backup_test_1
+    
+    clear_website_and_db
     restore_test_2
     verify_restore_test_2
-    #migrate_test_1
+    
+    clear_website_and_db
+    migrate_test_1
+    verify_migrate_test_1
     echo
 
     cleanup
--- unit_test/test_config/migrate_config.sh
+++ unit_test/test_config/migrate_config.sh
@@ -3,7 +3,7 @@
 # Keep all config values in single quotes unless specified otherwise
 
 #Feed the new domain name of your website
-NEW_DOMAIN='testa.vikilabs.org'
+NEW_DOMAIN='testd.vikilabs.org'
 NEW_DB_NAME='u218639858_testa_db'
 NEW_DB_USERNAME='u218639858_testa_admin'
 NEW_DB_PASSWORD='Test$DB123'
@@ -11,4 +11,4 @@ NEW_DB_HOST='localhost'
 NEW_DB_PORT='3306'
 
 #Do not keep the path inside quotes 
-NEW_WEBSITE_ROOT_DIR=~/public_html/
+NEW_WEBSITE_ROOT_DIR=~/public_html
