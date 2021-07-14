16c90a43ea185310d03fa57fee3a9f8c8790f9f8
Vignesh Natarajan <vickyemailbox@gmail.com>
2021-07-14 03:41:31 +0200

    export media code seperately

--- src/config.sh
+++ src/config.sh
@@ -3,30 +3,35 @@
 # Keep all config values in single quotes unless specified otherwise
 
 #enter the website address
-DOMAIN='testa.vikilabs.org'
+DOMAIN='test.com'
 
 #Keep the password inside single quotes ( Do not use double quotes )
 
-DB1_NAME='u218639858_testa_db'
-DB1_USERNAME='u218639858_testa_admin'
-DB1_PASSWORD='Test$DB123'
+DB1_NAME=''
+DB1_USERNAME=''
+DB1_PASSWORD=''
 
 DB2_NAME=''
 DB2_USERNAME=''
 DB2_PASSWORD=''
 
+#################
+
+DB_HOST='localhost'
+DB_PORT='3306'
+
 #OPTIONAL CONFIG [ SPECIAL PREVILAGES ]
 
-MYSQL_ADMIN_USERNAME=''
-MYSQL_ADMIN_PASSWORD=''
+MYSQL_ADMIN_USERNAME='root'
+MYSQL_ADMIN_PASSWORD='root'
 
 #website root directory ( don't keep / at the end)
 #Do not use quotes
-WEBSITE_ROOT_DIR=~/public_html/
+WEBSITE_ROOT_DIR=/var/www/html
 
 #website backup directory ( don't keep / at the end).
 #Do not use quotes
-BACKUP_DIR=~/test_backup
+BACKUP_DIR=/var/www/html
 
 #Use this config in double quotes
-LOG_FILE="/tmp/backup.testa.vikilabs.org.log"
+LOG_FILE="/tmp/test.log"
